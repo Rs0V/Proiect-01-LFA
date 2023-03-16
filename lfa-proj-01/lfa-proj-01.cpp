e16de847dacc6831948a554193a9e041ab7c4cbb
Rs0V <cris.al.iacov@gmail.com>
2023-03-16 02:30:57 +0200

    - programul functioneaza si pentru AFN-uri acum - adaugat macro-uri: STR, STR_LIST, WORDS, PRINT_SWITCH - modificat macro DICT - corectat algoritmul

--- lfa-proj-01/lfa-proj-01.cpp
+++ lfa-proj-01/lfa-proj-01.cpp
@@ -4,18 +4,23 @@
 #include <vector>
 #include <map>
 
-#define DICT std::map<std::pair<std::string, std::string>, std::string>
+#define PRINT_SWITCH 0
+
+#define STR std::string
+#define STR_LIST std::vector<STR>
+#define DICT std::map<std::pair<STR, STR>, STR_LIST>
+#define WORDS std::map<STR, STR_LIST>
 
 std::ifstream fin("input.in");
 
-std::string char2str(char c)
+STR char2str(const char c)
 {
-	std::string str;
+	STR str;
 	str.push_back(c);
 	return str;
 }
 
-int str2int(const std::string& str)
+int str2int(const STR& str)
 {
 	bool cast = false;
 	int number = 0;
@@ -32,9 +37,9 @@ int str2int(const std::string& str)
 	return cast ? number : -1;
 }
 
-std::vector<std::string> split_string(const std::string& str)
+STR_LIST split_string(const STR& str)
 {
-	std::vector<std::string> list;
+	STR_LIST list;
 	list.push_back("");
 	for (char c : str)
 		if (c == ' ')
@@ -44,15 +49,111 @@ std::vector<std::string> split_string(const std::string& str)
 	return list;
 }
 
+void automat_aux(const STR_LIST& end_states, const DICT& dict, const STR& input, const int input_index, const STR& curr_state, STR_LIST& state_path, WORDS& words)
+{
+	bool accepted = true;
+	if (input_index < input.size())
+	{
+		auto state_iter = dict.find({ curr_state, char2str(input[input_index]) });
+		if (state_iter != dict.end())
+		{
+			for (auto& state : state_iter->second)
+			{
+				state_path.push_back(state);
+				automat_aux(end_states, dict, input, input_index + 1, state, state_path, words);
+				state_path.pop_back();
+			}
+		}
+		else
+		{
+			auto word_iter = words.find(input);
+			if (word_iter == words.end())
+				words.insert({ input, STR_LIST() });
+		}
+	}
+	
+	if (accepted)
+	{
+		bool acc = false;
+		for (STR state : end_states)
+			if (curr_state == state)
+			{
+				acc = true;
+				break;
+			}
+		accepted = acc;
+	}
+	
+	STR subinput = input.substr(0, input_index);
+	auto word_iter = words.find(subinput);
+	if (word_iter == words.end())
+		if (accepted)
+			words.insert({ subinput, state_path });
+		else
+			words.insert({ subinput, STR_LIST() });
+	else if(word_iter->second.empty())
+		if (accepted)
+			word_iter->second = state_path;
+}
+
+void automat(STR& begin_state, STR_LIST& end_states, DICT& dict)
+{
+	bool run = true;
+	while (run)
+	{
+		STR input;
+		std::cin >> input;
+		if (input == "/stop" || input == ".stop")
+			run = false;
+		else
+		{
+			bool accepted = true;
+			STR curr_state = begin_state;
+			STR_LIST state_path;
+			state_path.push_back(curr_state);
+			WORDS words;
+
+			automat_aux(end_states, dict, input, 0, curr_state, state_path, words);
+
+			// Afiseaza pt toate cuv. (substring-uri din cuv. original) sau doar pentru cuv. original 
+			#if PRINT_SWITCH
+			for (auto& word : words)
+				if (word.first != "")
+					if (!word.second.empty())
+					{
+						std::cout << "cuvantul " << word.first << " este acceptat\ndrumul: ";
+						for (STR& state : word.second)
+							std::cout << state << ' ';
+						std::cout << "\n\n";
+					}
+					else
+						std::cout << "cuvantul " << word.first << " nu este acceptat\n\n";
+			#else
+			auto word = words.find(input);
+			if (word != words.end())
+				if (!word->second.empty())
+				{
+					std::cout << "cuvantul " << word->first << " este acceptat\ndrumul: ";
+					for (STR& state : word->second)
+						std::cout << state << ' ';
+					std::cout << "\n\n";
+				}
+				else
+					std::cout << "cuvantul " << word->first << " nu este acceptat\n\n";
+			#endif
+		}
+	}
+}
+
 int main()
 {
-	std::vector<std::string> input_lines;
-	std::string begin_state;
-	std::vector<std::string> end_states;
+	STR_LIST input_lines;
+	STR begin_state;
+	STR_LIST end_states;
 
 	while (!fin.eof())
 	{
-		std::string line;
+		STR line;
 		std::getline(fin, line);
 		input_lines.push_back(line);
 	}
@@ -61,8 +162,13 @@ int main()
 
 	for (int i = 0; i < input_lines.size() - 1; ++i)
 	{
-		std::vector<std::string> vstr = split_string(input_lines[i]);
-		dict.insert({ { vstr[0], vstr[1] }, vstr[2] });
+		STR_LIST vstr = split_string(input_lines[i]);
+		
+		auto state_iter = dict.find({ vstr[0], vstr[1] });
+		if (state_iter != dict.end())
+			state_iter->second.push_back(vstr[2]);
+		else
+			dict.insert({ { vstr[0], vstr[1] }, STR_LIST(1, vstr[2]) });
 
 		int bs_nr = str2int(begin_state);
 		int state0_nr = str2int(vstr[0]);
@@ -77,56 +183,7 @@ int main()
 	}
 	end_states = split_string(input_lines.back());
 
-	bool run = true;
-	while (run)
-	{
-		std::string input;
-		std::cin >> input;
-		if (input == "/stop")
-			run = false;
-		else
-		{
-			bool accepted = true;
-			std::string curr_state = begin_state;
-			std::vector<std::string> state_path;
-			state_path.push_back(curr_state);
-
-			for (char c : input)
-			{
-				auto state_iter = dict.find({ curr_state, char2str(c) });
-				if (state_iter != dict.end())
-				{
-					curr_state = state_iter->second;
-					state_path.push_back(curr_state);
-				}
-				else
-				{
-					accepted = false;
-					break;
-				}
-			}
-			if (accepted)
-			{
-				bool acc = false;
-				for (std::string state : end_states)
-					if (curr_state == state)
-					{
-						acc = true;
-						break;
-					}
-				accepted = acc;
-			}
-			if (accepted)
-			{
-				std::cout << "acceptat\ndrumul: ";
-				for (std::string state : state_path)
-					std::cout << state << ' ';
-				std::cout << "\n\n";
-			}
-			else
-				std::cout << "cuvantul nu este acceptat\n\n";
-		}
-	}
-
+	automat(begin_state, end_states, dict);
+	
 	return 0;
 }
